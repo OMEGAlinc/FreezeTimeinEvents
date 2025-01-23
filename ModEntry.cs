991ff8a222c85778fb5a6309b47ccbd24659a72a
OMEGAlinc <omegalinc8@gmail.com>
2025-01-22 19:20:12 -0500

    Initial upload

---
+++ ModEntry.cs
@@ -0,0 +1,27 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+
+namespace FreezeTimeinEvents
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            // Subscribe to game loop events
+            Helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
+        }
+
+        private void OnUpdateTicked(object? sender, StardewModdingAPI.Events.UpdateTickedEventArgs e)
+        {
+            // Check if an event (cutscene) is active
+            if (Game1.eventUp)
+            {
+                Game1.isTimePaused = true; // Freeze time during events
+            }
+            else
+            {
+                Game1.isTimePaused = false; // Unfreeze time when no event is active
+            }
+        }
+    }
+}
